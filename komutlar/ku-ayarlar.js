3548e51cf83fbc7a69b321c5745e0476b81d445e
Glitch (flat-caraway) <none>
2020-03-07 07:26:33 +0000

    💼🍇 Checkpoint ./komutlar/ku-ayarlar.js:3404493/404

--- komutlar/ku-ayarlar.js
+++ komutlar/ku-ayarlar.js
@@ -9,12 +9,12 @@ exports.run = (client, message, args) => {
 const embed = new Discord.RichEmbed()
 .setTitle(client.emojis.get("649690639054798848") + " » " + message.guild.name + " | Sunucu Ayarları «")
 .setColor("BLUE")
-.addField(`${db.has(`küfürE_${message.channel.id}`) ? `${client.emojis.get("649689234134925355")} Küfür` : `${client.emojis.get("649689229810728961")} Küfür `}`)
-.addField(`${db.has(`reklamK_${message.channel.id}`) ? `${client.emojis.get("649689234134925355")} Reklam` : `${client.emojis.get("649689229810728961")} Reklam `}`)
-.addField(`${db.has(`linkKR_${message.guild.id}`) ? `${client.emojis.get("649689234134925355")} Reklam Kick` : `${client.emojis.get("649689229810728961")} Reklam Kick`}`)
-.addField(`${db.has(`linkK_${message.channel.id}`) ? `${client.emojis.get("649689234134925355")} Link Engel` : `${client.emojis.get("649689229810728961")} Link Engel`}`)
-.addField(`${db.has(`linkK_${message.channel.id}`) ? `${client.emojis.get("649689234134925355")} Link Engel` : `${client.emojis.get("649689229810728961")} Link Engel`}`)
-.addField(`${client.emojis.get("649689234134925355")} Saldırı Koruma`)
+.addField(`${db.has(`küfürE_${message.channel.id}`) ? `${client.emojis.get("685749333228126229")} Küfür` : `${client.emojis.get("685749385321119744")} Küfür `}`)
+.addField(`${db.has(`reklamK_${message.channel.id}`) ? `${client.emojis.get("685749333228126229")} Reklam` : `${client.emojis.get("685749385321119744")} Reklam `}`)
+.addField(`${db.has(`linkKR_${message.guild.id}`) ? `${client.emojis.get("685749333228126229")} Reklam Kick` : `${client.emojis.get("685749385321119744")} Reklam Kick`}`)
+.addField(`${db.has(`linkK_${message.channel.id}`) ? `${client.emojis.get("685749333228126229")} Link Engel` : `${client.emojis.get("685749385321119744")} Link Engel`}`)
+.addField(`${db.has(`linkK_${message.channel.id}`) ? `${client.emojis.get("685749333228126229")} Link Engel` : `${client.emojis.get("685749385321119744")} Link Engel`}`)
+.addField(`${client.emojis.get("685749333228126229")} Saldırı Koruma`)
 message.channel.send(embed)
 };
 exports.conf = {

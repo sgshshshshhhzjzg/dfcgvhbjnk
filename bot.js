b7e58288e20f544c9be24890dac4e13d61c13500
Glitch (flat-caraway) <none>
2020-04-10 20:27:56 +0000

    🚏🐓 Checkpoint ./bot.js:3025281/8

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js'); const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
 const chalk = require('chalk');
 const moment = require('moment');

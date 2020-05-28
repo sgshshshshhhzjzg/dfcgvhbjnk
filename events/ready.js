160ee7e172ad0be6cc3dba704e80a3633f1b3275
Glitch (flat-caraway) <none>
2020-05-28 10:00:53 +0000

    🐟👮 Checkpoint ./events/ready.js:15612042/808

--- events/ready.js
+++ events/ready.js
@@ -14,8 +14,8 @@ module.exports = client => {
   //online = çevrimiçi
   console.log(`${client.user.id}                                                                                                                                                                     `)
   //client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
-client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
 
+client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
 //LISTENING = DİNLİYOR
   //WATCHING = İZLİYOR
   //PLAYING = OYNUYOR 

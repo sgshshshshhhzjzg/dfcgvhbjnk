3f1b09f35c0786a2a829ce99166fb3c195f86398
Glitch (flat-caraway) <none>
2020-05-31 09:29:19 +0000

    🚲⏰ Checkpoint ./komutlar/sil.js:15612042/16

--- komutlar/sil.js
+++ komutlar/sil.js
@@ -6,10 +6,10 @@ exports.run = async(client, message, args) => {
   
   
   const sayi = args[0]
-  if (sayi > 400) return message.reply("En Az `1 - 100` Arasında Bir Tam Sayı Değeri Girmelisiniz.")
+  if (sayi > 400) return message.reply("En Az `1 - 10000` Arasında Bir Tam Sayı Değeri Girmelisiniz.")
 
   let messages = await message.channel.fetchMessages({
-    limit: 400
+    limit: 10000
   });
 
      let mesaj = await message.channel.bulkDelete(messages, true);
